dcd30cc2e3f4932f687321f108e7dd127a72404e
Survivor22 <42820300+Survivor22@users.noreply.github.com>
2020-03-11 11:35:33 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,41 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main()
-{
-   string name,age,pob,attached;
-   int day,daycheck;
-
-    cout << "What is your name?" << endl;
-
-    cin>> name;
-
-    cout << "How old are you?" << endl;
-
-    cin >> age;
-
-    cout << "Where were you born?" << endl;
-
-    cin >> pob;
-
-    cout << "Which day of the month were you born" <<endl;
-
-    cin >> day;
-
-    if(0>=day || day>30){
-
-        cout<< "The date is not valid" <<endl;
-    }
-
-    else{
-
-        daycheck = day%10 ;
-
-        if(11< day && day< 14 || daycheck>3)
-
-         attached ="th";
-
-         else if(daycheck==1)
-
-          attached = "st";
-
-          else if(daycheck==2)
-
-          attached = "nd";
-
-
-           else if(daycheck==3)
-
-          attached = "rd";
-
-
-          cout<< "My name is " <<name<<". "<<"I am "<<age<<" years old"<<". "<<"I was born in "<<pob<<". "<<"I was born in the year "<<day<<" "<<attached<< " August,1999.";
-
-
-
-    }
-
-
-
-
-
-
-    return 0;
+void swapping(int &a, int &b) {      //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void bubbleSort(int *array, int size) {
+   for(int i = 0; i<size; i++) {
+      int swaps = 0;         //flag to detect any swap is there or not
+      for(int j = 0; j<size-i-1; j++) {
+         if(array[j] > array[j+1]) {       //when the current item is bigger than next
+            swapping(array[j], array[j+1]);
+            swaps = 1;    //set swap flag
+         }
+      }
+      if(!swaps)
+         break;       // No swap in this pass, so array is sorted
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];     //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   bubbleSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
 }
